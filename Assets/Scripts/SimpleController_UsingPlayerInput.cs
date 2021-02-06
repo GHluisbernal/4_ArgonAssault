a6d2e4ef6af8acb877070d94ee458711039e3781
Luis Bernal <luisenriquebernalfuentes@gmail.com>
2021-02-06 15:59:48 -0500

    Toggle Guns on command

--- Assets/Scripts/SimpleController_UsingPlayerInput.cs
+++ Assets/Scripts/SimpleController_UsingPlayerInput.cs
@@ -11,6 +11,7 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
     [SerializeField] private float xRange = 9f;
     [SerializeField] private float ySpeed = 10f;
     [SerializeField] private float yRange = 5f;
+    [SerializeField] private GameObject[] guns;
 
     [Header("Screen Position Based")]
     [SerializeField] private float positionPitchFactor = -5f;
@@ -22,13 +23,6 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
 
 
     private bool isControlEnabled = true;
-    private float rotateSpeed;
-    private float burstSpeed;
-    public GameObject projectile;
-
-    private bool m_Charging;
-    private Vector2 m_Rotation;
-    private Vector2 m_Look;
     private Vector2 m_Move;
 
     public void OnMove(InputAction.CallbackContext context)
@@ -36,34 +30,17 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
         m_Move = context.ReadValue<Vector2>();
     }
 
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        m_Look = context.ReadValue<Vector2>();
-    }
-
     public void OnFire(InputAction.CallbackContext context)
     {
+        //print("fire");
         switch (context.phase)
         {
-            case InputActionPhase.Performed:
-                if (context.interaction is SlowTapInteraction)
-                {
-                    StartCoroutine(BurstFire((int)(context.duration * burstSpeed)));
-                }
-                else
-                {
-                    Fire();
-                }
-                m_Charging = false;
-                break;
-
             case InputActionPhase.Started:
-                if (context.interaction is SlowTapInteraction)
-                    m_Charging = true;
+                ToggleGuns(true);
                 break;
 
             case InputActionPhase.Canceled:
-                m_Charging = false;
+                ToggleGuns(false);
                 break;
         }
     }
@@ -73,11 +50,11 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
         isControlEnabled = false;
     }
 
-    public void OnGUI()
-    {
-        if (m_Charging)
-            GUI.Label(new Rect(100, 100, 200, 100), "Charging...");
-    }
+    //public void OnGUI()
+    //{
+    //    if (m_Charging)
+    //        GUI.Label(new Rect(100, 100, 200, 100), "Charging...");
+    //}
 
     public void Update()
     {
@@ -87,19 +64,6 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
             ProccessTranslation(m_Move);
             ProccessRotation(m_Move);
         }
-    }
-
-    private void ProccessRotation(Vector2 direction)
-    {
-
-        var pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
-        var pitchDueToControlThrow = direction.y * controlPitchFactor;
-        var pitch = pitchDueToPosition + pitchDueToControlThrow;
-
-        var yaw = transform.localPosition.x * positionYawFactor;
-
-        var roll = direction.x * controlRollFactor;
-        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void ProccessTranslation(Vector2 direction)
@@ -116,36 +80,25 @@ public class SimpleController_UsingPlayerInput : MonoBehaviour
         transform.localPosition = new Vector3(xFuturePosition, yFuturePosition, transform.localPosition.z);
     }
 
-    private void Look(Vector2 rotate)
+
+    private void ProccessRotation(Vector2 direction)
     {
-        if (rotate.sqrMagnitude < 0.01)
-            return;
-        var scaledRotateSpeed = rotateSpeed * Time.deltaTime;
-        m_Rotation.y += rotate.x * scaledRotateSpeed;
-        m_Rotation.x = Mathf.Clamp(m_Rotation.x - rotate.y * scaledRotateSpeed, -89, 89);
-        transform.localEulerAngles = m_Rotation;
+
+        var pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        var pitchDueToControlThrow = direction.y * controlPitchFactor;
+        var pitch = pitchDueToPosition + pitchDueToControlThrow;
+
+        var yaw = transform.localPosition.x * positionYawFactor;
+
+        var roll = direction.x * controlRollFactor;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
-    private IEnumerator BurstFire(int burstAmount)
+    private void ToggleGuns(bool isActive)
     {
-        for (var i = 0; i < burstAmount; ++i)
+        foreach (var gun in guns)
         {
-            Fire();
-            yield return new WaitForSeconds(0.1f);
+            gun.SetActive(isActive);
         }
-    }
-
-    private void Fire()
-    {
-        var transform = this.transform;
-        var newProjectile = Instantiate(projectile);
-        newProjectile.transform.position = transform.position + transform.forward * 0.6f;
-        newProjectile.transform.rotation = transform.rotation;
-        const int size = 1;
-        newProjectile.transform.localScale *= size;
-        newProjectile.GetComponent<Rigidbody>().mass = Mathf.Pow(size, 3);
-        newProjectile.GetComponent<Rigidbody>().AddForce(transform.forward * 20f, ForceMode.Impulse);
-        newProjectile.GetComponent<MeshRenderer>().material.color =
-            new Color(Random.value, Random.value, Random.value, 1.0f);
     }
 }
